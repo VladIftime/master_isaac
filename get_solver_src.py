1c5284b25a8a59e69e4a7a7f20b5481dcd66dfa7
vladi <vladiftime@gmail.com>
2026-08-24 12:26:11 +0200

    small fixes

--- get_solver_src.py
+++
@@ -1,6 +0,0 @@
-from isaacsim import SimulationApp
-sim = SimulationApp({"headless": True})
-import inspect
-from isaacsim.robot.manipulators.articulation_kinematics_solver import ArticulationKinematicsSolver
-print(inspect.getsource(ArticulationKinematicsSolver))
-sim.close()
