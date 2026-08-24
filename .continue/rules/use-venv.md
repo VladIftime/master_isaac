b5ac5ade31a9318beddc44d258997e7037630332
vladi <vladiftime@gmail.com>
2026-08-24 12:24:11 +0200

    small fixes

--- .continue/rules/use-venv.md
+++
@@ -1,5 +0,0 @@
----
-description: use the correct venv for the isaac project
----
-
-use source ~/IsaacLab/master_isaac/.master_venv/bin/activate
--- pingpong_dual_arm/tasks/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Ping pong dual-arm environment."""
-
-from .pingpong_env_cfg import PingPongDualArmEnvCfg
-from .pingpong_env import PingPongEnv
